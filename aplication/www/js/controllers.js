90483afeecd9f7336d2ccf446fdcd00b7534452d
franheredia <franheredia.ov@gmail.com>
2016-11-24 01:17:56 -0300

    Code Improvements

--- aplication/www/js/controllers.js
+++ aplication/www/js/controllers.js
@@ -1,63 +1,172 @@
 angular.module('app.controllers', [])
 
-.controller('mainMenuCtrl', ['$scope', '$stateParams',
-  function($scope, $stateParams) {}
+.controller('mainMenuCtrl', ['$scope', '$stateParams', '$rootScope',
+  function($scope, $stateParams, $rootScope) {
+    $scope.createScopes = function() {
+      //Declaracion de equipos
+      $rootScope.teams = [{
+          name: 'Azul',
+          score: 0
+        }, {
+          name: 'Rojo',
+          score: 0
+        }]
+        //Declaracion de rondas
+      $rootScope.teamsPlays = [{
+        team: $rootScope.teams[0],
+        round: 0,
+        played: false,
+      }, {
+        team: $rootScope.teams[1],
+        round: 0,
+        played: false,
+      }, {
+        team: $rootScope.teams[0],
+        round: 1,
+        played: false,
+      }, {
+        team: $rootScope.teams[1],
+        round: 1,
+        played: false,
+      }, {
+        team: $rootScope.teams[0],
+        round: 2,
+        played: false,
+      }, {
+        team: $rootScope.teams[1],
+        round: 2,
+        played: false,
+      }];
+      //Declaracion de Temas
+      $rootScope.topics = [{
+        name: 'Personajes',
+        icon: 'icon ion-android-people',
+        type: 'character',
+        selected: false
+      }, {
+        name: 'Videojuegos',
+        icon: 'icon ion-ios-game-controller-b',
+        type: 'videogame',
+        selected: false
+      }, {
+        name: 'Películas',
+        icon: 'icon ion-android-film',
+        type: 'movie',
+        selected: false
+      }, {
+        name: 'Lugares',
+        icon: 'icon ion-android-globe',
+        type: 'place',
+        selected: false
+      }];
+      //Declaracion de Palabras
+      $rootScope.words = [{
+        type: 'videogame',
+        icon: 'icon ion-ios-game-controller-b',
+        name: 'Super Mario Bros',
+        description: 'Videojuego de Plataformas',
+        bannedWords: ['Fontanero', 'Bigote', 'Princesa']
+      }, {
+        type: 'videogame',
+        icon: 'icon ion-ios-game-controller-b',
+        name: 'Skyrim',
+        description: 'Videojuego de Rol',
+        bannedWords: ['Dragón', 'Sangre', 'Grito']
+      }, {
+        type: 'videogame',
+        icon: 'icon ion-ios-game-controller-b',
+        name: 'Age of Empires',
+        description: 'Videojuego de Estrategia',
+        bannedWords: ['Imperio', 'Edades', 'Centro Urbano']
+      }, {
+        type: 'character',
+        icon: 'icon ion-android-people',
+        name: 'Joseph Stallin',
+        description: 'Personaje Historico',
+        bannedWords: ['Bigote', 'Comunismo', 'Rusia']
+      }, {
+        type: 'character',
+        icon: 'icon ion-android-people',
+        name: 'Superman',
+        description: 'Personaje de Ficcion',
+        bannedWords: ['Kriptonita', 'Capa', 'Calzonsillo']
+      }, {
+        type: 'character',
+        icon: 'icon ion-android-people',
+        name: 'Donald Trump',
+        description: 'Personaje Público',
+        bannedWords: ['Estados Unidos', 'Yanqui', 'Anaranjado']
+      }, {
+        type: 'movie',
+        icon: 'icon ion-android-film',
+        name: 'El Sexto Sentido',
+        description: 'Pelicula de Suspenso',
+        bannedWords: ['Fantasmas', 'Muerte', 'Gente']
+      }, {
+        type: 'movie',
+        icon: 'icon ion-android-film',
+        name: 'Kung-Fu Panda',
+        description: 'Película Animada',
+        bannedWords: ['China', 'Fideos', 'Guerrero']
+      }, {
+        type: 'movie',
+        icon: 'icon ion-android-film',
+        name: 'Titanic',
+        description: 'Película Romantica',
+        bannedWords: ['Iceberg', 'Barco', 'Hundir']
+      }, {
+        type: 'place',
+        icon: 'icon ion-android-globe',
+        name: 'Torre Eifel',
+        description: 'Monumento Francés',
+        bannedWords: ['París', 'Vino', 'Mimo']
+      }, {
+        type: 'place',
+        icon: 'icon ion-android-globe',
+        name: 'Las Vegas',
+        description: 'Estado Norteamericano',
+        bannedWords: ['Casino', 'Pecado', 'Apostar']
+      }, {
+        type: 'place',
+        icon: 'icon ion-android-globe',
+        name: 'Chernóbil',
+        description: 'Ciudad Rusa',
+        bannedWords: ['Radiación', 'Planta Nuclear', 'Fantasma']
+      }];
+      //Declaracion de las palabras en juego
+      $rootScope.playingWords = [];
+      //Declaracion de las respuestas incorrectas en juego
+      $rootScope.wrongAswers = [];
+      //Declaracion de la funcion getRandom()
+      $rootScope.getRandom = function(maxExclusive, minInclusive = 0) {
+        randomNum = Math.floor(Math.random() * (maxExclusive - minInclusive) + minInclusive);
+        return (randomNum);
+      }
+    }
+  }
 ])
 
 .controller('chooseTopicsCtrl', ['$scope', '$stateParams', '$rootScope',
   function($scope, $stateParams, $rootScope) {
-    $scope.topics = [{
-      name: 'Personajes',
-      icon: 'icon ion-android-people',
-      type: 'character'
-    }, {
-      name: 'Videojuegos',
-      icon: 'icon ion-ios-game-controller-b',
-      type: 'videogame'
-    }, {
-      name: 'Películas',
-      icon: 'icon ion-android-film',
-      type: 'movie'
-    }, {
-      name: 'Lugares',
-      icon: 'icon ion-android-globe',
-      type: 'place'
-    }];
-    $rootScope.selected = [];
-
-    $scope.clicked = function(member) {
-      var index = $rootScope.selected.indexOf(member);
-      if (index >= 0) {
-        $rootScope.selected.splice(index, 1);
-        member.selected = false;
-      } else {
-        $rootScope.selected.push(member);
-        member.selected = true;
-      }
+    //Seleccion de Temas
+    $scope.clicked = function(selection) {
+      value = selection['selected'];
+      selection['selected'] = !value;
     }
-
   }
 ])
 
 .controller('waitingTeamCtrl', ['$scope', '$stateParams', '$rootScope',
   function($scope, $stateParams, $rootScope) {
 
-    $rootScope.teams = [{
-      name: 'Azul',
-      score: 0
-    }, {
-      name: 'Rojo',
-      score: 0
-    }]
   }
 ])
 
 .controller('playingCtrl', ['$scope', '$stateParams', '$rootScope', '$timeout', '$state',
-
   function($scope, $stateParams, $rootScope, $timeout, $state) {
-    $scope.counter = 30;
+    //Declaracion de Funciones del cronometro
     var mytimeout = null;
-
+    $scope.counter = 30;
     $scope.onTimeout = function() {
       if ($scope.counter === 1) {
         $scope.$broadcast('timer-stopped', 0);
@@ -73,122 +182,38 @@ angular.module('app.controllers', [])
         $state.go('positions');
       }
     });
-
-    $scope.words = [{
-      type: 'videogame',
-      icon: 'icon ion-ios-game-controller-b',
-      name: 'Super Mario Bros',
-      description: 'Videojuego de Plataformas',
-      bannedWords: ['Fontanero', 'Bigote', 'Princesa']
-    }, {
-      type: 'videogame',
-      icon: 'icon ion-ios-game-controller-b',
-      name: 'Skyrim',
-      description: 'Videojuego de Rol',
-      bannedWords: ['Dragón', 'Sangre', 'Grito']
-    }, {
-      type: 'videogame',
-      icon: 'icon ion-ios-game-controller-b',
-      name: 'Age of Empires',
-      description: 'Videojuego de Estrategia',
-      bannedWords: ['Imperio', 'Edades', 'Centro Urbano']
-    }, {
-      type: 'character',
-      icon: 'icon ion-android-people',
-      name: 'Joseph Stallin',
-      description: 'Personaje Historico',
-      bannedWords: ['Bigote', 'Comunismo', 'Rusia']
-    }, {
-      type: 'character',
-      icon: 'icon ion-android-people',
-      name: 'Superman',
-      description: 'Personaje de Ficcion',
-      bannedWords: ['Kriptonita', 'Capa', 'Calzonsillo']
-    }, {
-      type: 'character',
-      icon: 'icon ion-android-people',
-      name: 'Donald Trump',
-      description: 'Personaje Público',
-      bannedWords: ['Estados Unidos', 'Yanqui', 'Anaranjado']
-    }, {
-      type: 'movie',
-      icon: 'icon ion-android-film',
-      name: 'El Sexto Sentido',
-      description: 'Pelicula de Suspenso',
-      bannedWords: ['Fantasmas', 'Muerte', 'Gente']
-    }, {
-      type: 'movie',
-      icon: 'icon ion-android-film',
-      name: 'Kung-Fu Panda',
-      description: 'Película Animada',
-      bannedWords: ['China', 'Fideos', 'Guerrero']
-    }, {
-      type: 'movie',
-      icon: 'icon ion-android-film',
-      name: 'Titanic',
-      description: 'Película Romantica',
-      bannedWords: ['Iceberg', 'Barco', 'Hundir']
-    }, {
-      type: 'place',
-      icon: 'icon ion-android-globe',
-      name: 'Torre Eifel',
-      description: 'Monumento Francés',
-      bannedWords: ['París', 'Vino', 'Mimo']
-    }, {
-      type: 'place',
-      icon: 'icon ion-android-globe',
-      name: 'Las Vegas',
-      description: 'Estado Norteamericano',
-      bannedWords: ['Casino', 'Pecado', 'Apostar']
-    }, {
-      type: 'place',
-      icon: 'icon ion-android-globe',
-      name: 'Chernóbil',
-      description: 'Ciudad Rusa',
-      bannedWords: ['Radiación', 'Planta Nuclear', 'Fantasma']
-    }];
-    var playingWords = [];
-    var wrongAswers = [];
-
-    function getRandom(max) {
-      var randomNum = Math.floor(Math.random() * (max));
-      return (randomNum);
-    }
-
+    //Declaracion de la funcion chargeCard()
     function chargeCard(targetArray) {
-      x = getRandom(playingWords.length);
-      $scope.current = playingWords[x];
+      x = $rootScope.getRandom($rootScope.playingWords.length);
+      $scope.current = $rootScope.playingWords[x];
     }
-
+    //Declaracion de la funcion que se ejecuta al comenzar la partida
     $scope.gameStarted = function() {
-      for (x = 0; x < $rootScope.selected.length; x++) {
-        for (u = 0; u < $scope.words.length; u++) {
-          if ($scope.words[u]['type'] == $rootScope.selected[x]['type']) {
-            playingWords.push($scope.words[u]);
+        for (u = 0; u < $rootScope.topics.length; u++) {
+          if ($rootScope.topics[u]['selected']) {
+            for (i = 0; i < $rootScope.words.length; i++) {
+              if ($rootScope.topics[u]['type'] == $rootScope.words[i]['type']) {
+                $rootScope.playingWords.push($rootScope.words[i]);
+              }
+            }
           }
         }
+        chargeCard($rootScope.playingWords);
+        mytimeout = $timeout($scope.onTimeout, 1000);
       }
-      chargeCard(playingWords);
-      mytimeout = $timeout($scope.onTimeout, 1000);
-    }
-
+      //Declaracion de la funcion de cargar otra tarjeta nextCard()
     $scope.nextCard = function(answerBoolean) {
-      for (x = 0; x < playingWords.length; x++) {
-        if (playingWords[x] == $scope.current) {
-          playingWords.splice(x, 1);
-          if (answerBoolean) {
-            $rootScope.teams[0]['score']++;
-          } else {
-            wrongAswers.push($scope.current);
-          }
-        }
+      currentWord = $rootScope.playingWords.indexOf($scope.current);
+      $rootScope.playingWords.splice(currentWord, 1);
+      if (answerBoolean) {
+        $rootScope.teams[0]['score']++;
+      } else {
+        $rootScope.wrongAswers.push($scope.current);
       }
-      if (playingWords.length == 0) {
+      if ($rootScope.playingWords.length == 0) {
         $state.go('positions');
       }
-      chargeCard(playingWords);
-      console.log($rootScope.teams[0]['score']);
-
+      chargeCard($rootScope.playingWords);
     }
   }
 ])
